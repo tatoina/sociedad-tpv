ac89e119604f6bb79ae99d2955a212f1b3469ed4
LaToalla <latoallaapp@gmail.com>
2025-12-15 14:24:13 +0100

    Cambiar etiqueta de Listados Gastos a Listados TPV para admin

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -74,7 +74,7 @@ export default function Menu({ user, profile }) {
         {!profile?.isAdmin && <MenuItem to="/tpv" icon="🛍️" label="TPV" />}
         {!profile?.isAdmin && <MenuItem to="/eventos" icon="📅" label="Eventos" />}
         {!profile?.isAdmin && <MenuItem to="/listados-eventos" icon="📊" label="Listados Eventos" />}
-        {profile?.isAdmin && <MenuItem to="/listados-tpv" icon="💰" label="Listados Gastos" isAdmin />}
+        {profile?.isAdmin && <MenuItem to="/listados-tpv" icon="💰" label="Listados TPV" isAdmin />}
         {profile?.isAdmin && <MenuItem to="/productos" icon="📦" label="Productos" isAdmin />}
         {profile?.isAdmin && <MenuItem to="/socios" icon="👥" label="Socios" isAdmin />}
       </div>
